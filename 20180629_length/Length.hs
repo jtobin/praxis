f20599606d97c531d260ce866980975d358cecb8
Jared Tobin <jared@jtobin.ca>
2018-07-02 11:09:50 +1200

    Add length exercise.

---
+++ 20180629_length/Length.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
+
+import Prelude hiding (compare)
+
+len :: [a] -> Int
+len = loop 0 where
+  loop !acc = \case
+    []    -> acc
+    (h:t) -> loop (succ acc) t
+
+compare :: [a] -> [b] -> Ordering
+compare = loop where
+  loop l r = case (l, r) of
+    (h : _, []) -> GT
+    ([], h : _) -> LT
+    ([], [])    -> EQ
+    _           -> loop (drop 1 l) (drop 1 r)
+
+main :: IO ()
+main = do
+  print (len [1..1000])
+  print (compare [1..10] [1..])
